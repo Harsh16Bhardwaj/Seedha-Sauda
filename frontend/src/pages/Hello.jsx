69825f66754bee1ae1c5b18ea7056feac56fd02f
Harsh Bhardwaj <bhardwajharshmait23@gmail.com>
2025-02-20 09:09:55 +0530

    "DEplyoment Changes"

--- frontend/src/pages/Hello.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-
-export default function Hello() {
-    return (<>
-        <div className="bg-black h-screen">Hello</div>
-        <div className="bg-blue-950 h-screen">Hello2</div>
-    </>)
-}
